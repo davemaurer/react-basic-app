49036dc1af50aa3e84b166ea143d2c4bdcfec7f2
David Maurer <davemaurermail@gmail.com>
2017-08-04 16:19:55 -0600

    move StorePicker component to its own file.

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,10 +1,6 @@
-import React from 'react';
+import React from 'react'; // no need for a file path because webpack looks in nodemodules dir for you if it's just a string.
 import { render } from 'react-dom';
+import StorePicker from './components/StorePicker'; // need to give relative file path for components the YOU make.
 
-class StorePicker extends React.Component {
-  render() {
-    return <p>Hello</p>
-  }
-}
 
 render(<StorePicker/>, document.querySelector('#main'));
