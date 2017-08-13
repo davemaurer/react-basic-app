ca0566791aa3b1c8104c344ba232f46638f756f2
David Maurer <davemaurermail@gmail.com>
2017-08-13 13:08:38 -0600

    add html to StorePicker component

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render() {
-    return <p>Hello</p>
+    // return React.createElement('h1', {className: 'big-stuff'}, 'This is a big font'); **this would be WITHOUT JSX
+    return (
+      // You must always return only ONE element/parent-tag, so nest things if you need to have more than one tag returned
+      <form className="store-selector">
+        <h2>Please Enter a Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store</button>
+      </form>
+    )
   }
 }
 
