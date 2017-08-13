3a79177e659342ccae66940ea74c05aa1f01fd3c
David Maurer <davemaurermail@gmail.com>
2017-08-13 13:17:00 -0600

    import css to index.js

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'; // no need for a file path because webpack looks in nodemodules dir for you if it's just a string.
 import { render } from 'react-dom';
+import './css/style.css'
 import StorePicker from './components/StorePicker'; // need to give relative file path for components the YOU make.
 
 
