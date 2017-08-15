48c1c9892b4863d9b701d33113c95e9cf33631ab
David Maurer <davemaurermail@gmail.com>
2017-08-14 23:04:35 -0600

    add event listener and example of how to bind this to functions outside of a component's render function if you have multiple outside functions.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,15 +2,33 @@ import React from 'react';
 import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+  // If reusing *this* with multiple methods inside of a component, it's helpful to bind *this* with all
+  // of those methods using the following.
+  // constructor() {
+  //  super();
+  //    this.goToStore = this.goToStore.bind(this);
+  //    this.mySecondMethod = this.goToStore.bind(this);
+  //    and so on
+  //  }
+  
+
+  goToStore(event) {
+    event.preventDefault(); // stops page refresh, which is default behavior when form is submitted
+    // 1 - Grab the text from the box on the page
+    console.log(this.storeInput);
+    // 2 - Transition fro / to /store/:storeId
+  }
+
   render() {
     // return React.createElement('h1', {className: 'big-stuff'}, 'This is a big font'); **this would be WITHOUT JSX
     return (
       // You must always return only ONE element/parent-tag, so nest things if you need to have more than one tag returned
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         { /* Inside of JSX element, you need to use this method. Regular comment convention doesn't work */ }
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
-        <button type="submit">Visit Store</button>
+        <input type="text" required placeholder="Store Name" defaultValue={
+          getFunName() } ref={ (input) => { this.storeInput = input } } />
+        <button type="submit">Visit Store -></button>
       </form>
     )
   }
