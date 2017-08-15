629d435ce050d97220d6f2ec6e5c676aecf1c3e7
David Maurer <davemaurermail@gmail.com>
2017-08-14 22:44:17 -0600

    add routing using react-router library. use helper method for random names

---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <h12>Not Found!111!!</h12>
+    )
+  }
+}
+
+export default NotFound;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,23 @@
 import React from 'react'; // no need for a file path because webpack looks in nodemodules dir for you if it's just a string.
+import { BrowserRouter, Match, Miss } from 'react-router';
 import { render } from 'react-dom';
-import './css/style.css'
-import StorePicker from './components/StorePicker'; // need to give relative file path for components the YOU make.
-import App from './components/App';
 
-render(<App/>, document.querySelector('#main'));
+import './css/style.css'
+import App from './components/App';
+import NotFound from './components/NotFound';
+
+import StorePicker from './components/StorePicker'; // need to give relative file path for components the YOU make.
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+};
+
+render(<Root/>, document.querySelector('#main'));
