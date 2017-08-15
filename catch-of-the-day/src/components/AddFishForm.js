d578e149982c588fdc6ebc89c24179673fb566f9
David Maurer <davemaurermail@gmail.com>
2017-08-15 10:49:51 -0600

    set up state on the App component so data can be passed down to child components

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -3,20 +3,26 @@ import React from 'react';
 class AddFishForm extends React.Component {
   createFish(event) {
     event.preventDefault();
-    console.log("this is working");
+    const fish = {
+      name: this.name.value,
+      price: this.price.value,
+      status: this.status.value,
+      desc: this.desc.value,
+      image: this.image.value
+    }
   }
 
   render() {
     return (
       <form className="fish-edit" onSubmit={ (e) => this.createFish(e) }>
-        <input type="text" placeholder="Fish Name"/>
-        <input type="text" placeholder="Fish Price"/>
+        <input ref={ (input) => this.name = input } type="text" placeholder="Fish Name"/>
+        <input ref={ (input) => this.price = input } type="text" placeholder="Fish Price"/>
         <select>
           <option value="available">Fresh!</option>
           <option value="unavailable">Sold Out!</option>
         </select>
-        <textarea placeholder="Fish Desc"/>
-        <input type="text" placeholder="Fish Image"/>
+        <textarea ref={ (input) => this.desc = input } placeholder="Fish Desc"/>
+        <input ref={ (input) => this.image = input } type="text" placeholder="Fish Image"/>
         <button type="submit">+ Add Item</button>
       </form>
     )
