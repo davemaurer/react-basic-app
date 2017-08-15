3caff20425b603eb4f0f2a89b5c6f727e149b172
David Maurer <davemaurermail@gmail.com>
2017-08-15 17:19:43 -0600

    pass addFish function down to AddFishForm via props.

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -6,11 +6,24 @@ import Inventory from './Inventory';
 class App extends React.Component {
   constructor() {
     super();
+    this.addFish = this.addFish.bind(this);
+    // get initial state
     this.state = {
       fishes: {},
       order: {}
     };
   }
+
+  addFish(fish) {
+    // update the state
+    const fishes = {...this.state.fishes};
+    // add the new fish using a timestamp as the order decider
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // set the state
+    this.setState({ fishes })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -18,7 +31,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
