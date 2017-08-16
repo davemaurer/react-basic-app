9125c38f4d7c083b1813114dd3948aaf67a4391d
David Maurer <davemaurermail@gmail.com>
2017-08-15 21:04:02 -0600

    set up firebase for maintaining persistent state

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,10 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyB2_a5oVJtcnRNgu9-j88m_8jYPzO7O19o",
+  authDomain: "catch-two.firebaseapp.com",
+  databaseURL: "https://catch-two.firebaseio.com"
+});
+
+
+export default base;
