b74fb0a1e235795df71b72d2219a983b73e6b2ab
David Maurer <davemaurermail@gmail.com>
2017-08-17 11:20:14 -0600

    use delete in removeFromOrder function instead of setting the key to null so it will correctly remove the fish from the order

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -89,7 +89,7 @@ class App extends React.Component {
 
   removeFromOrder(key) {
     const order = {...this.state.order};
-    order[key] = null;
+    delete order[key];
     this.setState({ order });
   }
 
